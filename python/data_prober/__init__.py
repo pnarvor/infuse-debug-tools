ed9463a1cb74f7308d8fbb734af29f84395d2adf
Pierre Narvor <pnarvor@laas.fr>
2019-04-24 18:06:55 +0200

    [data_prober] Made a proper python package

---
+++ python/data_prober/__init__.py
@@ -0,0 +1,7 @@
+from .MetadataFormat      import MetadataFormat
+from .Metadata            import Metadata
+from .BrokenImageDetector import BrokenImageDetector
+from .ImageSynchronizer   import StereoPairStamp
+from .ImageSynchronizer   import ImageSynchronizer
+from .DataCleaner         import DataCleaner
+
