184b84fd78628aa0147bbf0f0ee4f252a2186066
Pierre Narvor <pnarvor@laas.fr>
2019-05-23 15:10:01 +0200

    [export] Fixed export paths of cameras in DataCleaner2

--- python/data_prober/DataCleaner2.py
+++ python/data_prober/DataCleaner2.py
@@ -19,9 +19,9 @@ class DataCleaner2:
 
         self.robotPoseData = RobotPoseData(dataRootDir)
         self.velodyneData  = VelodyneData(dataRootDir, exportPath)
-        self.navData       = CameraData(dataRootDir, "nav")
-        self.frontData     = CameraData(dataRootDir, "front")
-        self.rearData      = CameraData(dataRootDir, "rear")
+        self.navData       = CameraData(dataRootDir, "nav"  , exportPath)
+        self.frontData     = CameraData(dataRootDir, "front", exportPath)
+        self.rearData      = CameraData(dataRootDir, "rear" , exportPath)
 
         # Global data
         self.minTime       = -1
@@ -30,9 +30,9 @@ class DataCleaner2:
 
         self.robotPoseData.load()
         self.velodyneData.load()
-        self.navData.load()
-        self.frontData.load()
-        self.rearData.load()
+        # self.navData.load()
+        # self.frontData.load()
+        # self.rearData.load()
 
         self.compute_mission_time()
 
@@ -40,17 +40,24 @@ class DataCleaner2:
         self.velodyneData.tag_odometry(self.robotPoseData)
         self.velodyneData.suggest_broken_data()
 
-        self.navData.compute_retagged_poses(self.robotPoseData)
-        self.navData.tag_odometry(self.robotPoseData)
-        self.navData.suggest_broken_data()
+        # self.navData.compute_retagged_poses(self.robotPoseData)
+        # self.navData.tag_odometry(self.robotPoseData)
+        # self.navData.suggest_broken_data()
 
-        self.frontData.compute_retagged_poses(self.robotPoseData)
-        self.frontData.tag_odometry(self.robotPoseData)
-        self.frontData.suggest_broken_data()
+        # self.frontData.compute_retagged_poses(self.robotPoseData)
+        # self.frontData.tag_odometry(self.robotPoseData)
+        # self.frontData.suggest_broken_data()
 
-        self.rearData.compute_retagged_poses(self.robotPoseData)
-        self.rearData.tag_odometry(self.robotPoseData)
-        self.rearData.suggest_broken_data()
+        # self.rearData.compute_retagged_poses(self.robotPoseData)
+        # self.rearData.tag_odometry(self.robotPoseData)
+        # self.rearData.suggest_broken_data()
+
+    def export(self):
+
+        self.velodyneData.export()
+        # self.navData.export()
+        # self.frontData.export()
+        # self.rearData.export()
 
     def compute_mission_time(self):
 
@@ -72,8 +79,8 @@ class DataCleaner2:
 
     def display(self, verbose=False, blocking=False):
 
-        self.robotPoseData.display(verbose, blocking)
+        # self.robotPoseData.display(verbose, blocking)
         self.velodyneData.display(verbose, blocking)
-        self.navData.display(verbose, blocking)
-        self.frontData.display(verbose, blocking)
-        self.rearData.display(verbose, blocking)
+        # self.navData.display(verbose, blocking)
+        # self.frontData.display(verbose, blocking)
+        # self.rearData.display(verbose, blocking)
